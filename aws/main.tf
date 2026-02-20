cbdb3a0c4ef3efa1de0a5ac2eb96b62f13d15d3a
ghatoleakruti <ghatole.a@northeastern.edu>
2026-02-19 20:28:45 -0500

    testing ci

--- aws/main.tf
+++ aws/main.tf
@@ -64,17 +64,3 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${local.name}-private-rt"
-  }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
--- aws/ssh-key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "main" {
-  key_name   = "${var.project}-${var.env}-${var.name_suffix}-key"
-  public_key = var.ssh_public_key
-}
